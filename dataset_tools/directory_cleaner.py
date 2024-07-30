a28ac26401552728a4c27e950b4f25c8b228b8c8
kaufmantw <kaufmantw@jacks.sfasu.edu>
2024-07-30 04:22:01 -0500

    script for removing files
    Files not in the whitelist get removed

---
+++ dataset_tools/directory_cleaner.py
@@ -0,0 +1,35 @@
+import os
+from collections import Counter
+
+def list_files(files):
+    # Extract file extensions and count them
+    extensions = [os.path.splitext(file)[1] for file in files]
+    extension_counts = Counter(extensions)
+
+    # Print the counts
+    for ext, count in extension_counts.items():
+        print(f'{ext}: {count}')
+
+# Define the directory you want to scan
+directory = 'C:\\Users\\timka\\Documents\\code\\python\\Tektim-Bot\\data\\images\\break-room_media'
+
+whitelist = ['.jpeg','.png']
+
+# List all files in the directory
+files = os.listdir(directory)
+
+list_files(files)
+
+for file in files:
+    extension = os.path.splitext(file)[1]
+    if extension not in whitelist:
+        file_path = directory + "\\" + file
+        #print(file_path)
+        try:
+            os.remove(file_path)
+            print("Removed: " + file_path)
+        except Exception as e:
+            print(f'An error has occured: {e}')
+
+files = os.listdir(directory)
+list_files(files)
