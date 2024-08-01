04f47300103ef61d7b004fde5b6607ac84c33ba4
kaufmantw <kaufmantw@jacks.sfasu.edu>
2024-08-01 06:40:28 -0500

    Image Prediction
    Read in images from discord messages and predict using the bot

--- controller.py
+++ controller.py
@@ -3,6 +3,16 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
+import joblib
+import requests
+import shutil
+import os
+
+#image prediction and processing imports
+from sklearn.svm import SVC
+from skimage.io import imread
+from skimage.transform import resize
+from skimage.color import rgb2gray
 
 def generate_response(msg):
     '''
@@ -26,21 +36,80 @@ def generate_response(msg):
         response = random.choice(msg_options)
     return response
 
-def generate_react_on_media(img):
+def generate_react_on_media(attachment):
     #If an image is sent, for now respond with an emote. 10 10 80 split
-    rng = random.randrange(1, 10)
+    # catchest:        <:catchest:1267111295145087057>
+    # erm:             <:erm:1267111273275854908>
+    # golden catchest: <:golden_catchest:1268418504990654546>
+
     reaction = ''
-    match rng:
-        case 1:
-            reaction = "<:catchest:1267111295145087057>"
-        case 2:
-            reaction = "<:erm:1267111273275854908>"
-        case 3:
-            rng2 = random.rangrange(1, 100)
-            if rng2 == 1:
-                reaction = "<:golden_catchest:1268418504990654546>"
+
+    # if there is more than one attachment, just look at the first one
+    if len(attachment) > 1:
+        attachment = attachment[0]
+
+    # if the attachment is an image, we are going to predict on it
+    if attachment.content_type.startswith('image/'):
+
+        #download the image
+        download_image(attachment.url, attachment.filename)
+        path = "C:\\Users\\timka\\Documents\\code\\python\\Tektim-Bot\\data\\images\\live_input\\" + attachment.filename 
+        img = imread(path)[:,:,:3] #use only first three channels
+
+        #reformat the image
+        img = resize(img, (256, 256))
+        img = rgb2gray(img)
+        x_test = img.flatten()
+
+        #grab the model
+        model = joblib.load('data/models/256img_model.joblib')
+
+        #predict using the model
+        prediction = model.predict(x_test)
+
+        # assign reaction off prediction
+        match prediction:
+            case "funny":
+                rng2 = random.randrange(1, 100)
+                if rng2 == 1:
+                    reaction = "<:golden_catchest:1268418504990654546>"
+                else:
+                    reaction = "<:catchest:1267111295145087057>"
+            case "cringe":
+                reaction = "<:erm:1267111273275854908>"
+
+    # if the attachment is not an image, do default reacting
+    else:
+        rng = random.randrange(1, 10)
+        match rng:
+            case 1:
+                rng2 = random.randrange(1, 100)
+                if rng2 == 1:
+                    reaction = "<:golden_catchest:1268418504990654546>"
+                else:
+                    reaction = "<:catchest:1267111295145087057>"
+            case 2:
+                reaction = "<:erm:1267111273275854908>"
+
+    # when prediction is done, delete the image from live_input
+    os.remove(path)
 
     return reaction
+
+def download_image(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        dir = "C:\\Users\\timka\\Documents\\code\\python\\Tektim-Bot\\data\\images\\live_input"
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        print(f'Downloaded {filename}')
+
+        # move the file to the correct folder
+        shutil.move(filename, dir)
+
+    else:
+        print(f'Failed to download {filename}')
+
 
 nltk.download('stopwords')
 nltk.download('punkt')
