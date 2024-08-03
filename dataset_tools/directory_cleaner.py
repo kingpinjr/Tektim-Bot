138bd0d6369bb50298d0883d14509d1d02f2726b
kaufmantw <kaufmantw@jacks.sfasu.edu>
2024-08-03 00:19:23 -0500

    directory cleaner removal
    Removed size consideration for images, script now also removes temporary directories for image imports

--- dataset_tools/directory_cleaner.py
+++ dataset_tools/directory_cleaner.py
@@ -13,31 +13,34 @@ def list_file_counts(files):
     for ext, count in extension_counts.items():
         print(f'{ext}: {count}')
 
-img_size = 256
-col_num = img_size ** 2
+#img_size = 256
+#col_num = img_size ** 2
 
 prep_dir = 'data\\images\\preprocessed'
-print('Before:')
+print('Images in Preprocessed Before:')
 files = os.listdir(prep_dir)
 list_file_counts(files)
 
 # Define the directory you want to scan
-directory = 'C:\\Users\\timka\\Documents\\code\\python\\Tektim-Bot\\data\\images'
+parent_directory = 'data\\images'
 
 ext_whitelist = ['.jpeg','.png','.jpg']
 
 dir_blacklist = ('data\\images\\processed',
                  'data\\images\\preprocessed',
-                 'data\\images\\live_input')
+                 'data\\images\\live_input',
+                 'data\\images\\cringe',
+                 'data\\images\\neutral',
+                 'data\\images\\funny')
 
 # List all files in the directory
 #files = os.listdir(directory)
-for root, dirs, files in os.walk(directory, topdown=False):
+for root, dirs, files in os.walk(parent_directory, topdown=False):
     for file in files:
         if not root.endswith(dir_blacklist): # all files that aren't in blacklisted directories
             extension = os.path.splitext(file)[1]
             # filter based off of file type
-            file_path = directory + "\\" + file
+            file_path = root + "\\" + file
             if extension not in ext_whitelist:
                 #print(file_path)
                 try:
@@ -47,21 +50,20 @@ for root, dirs, files in os.walk(directory, topdown=False):
                     print(f'An error has occured: {e}')
             
             else:
-                img = imread(file_path)
-                img = resize(img, (img_size, img_size))
-                img = img.flatten()
-                # filter bsaed off size of file; only accept files that flatten to be 900
-                if(len(img) != col_num):
-                    try:
-                        os.remove(file_path)
-                        print("Removed: " + file_path)
-                    except Exception as e:
-                        print(f'An error has occured: {e}')
-                
-                else:
-                    shutil.move(file_path, prep_dir)
+                shutil.move(file_path, prep_dir)
+
+
                 
 
-print('\nAfter:')
+print('\nImages in Preprocessed After:')
 files = os.listdir(prep_dir)
 list_file_counts(files)
+
+# delete emptied image folders now
+subdirectories = next(os.walk(parent_directory))[1]
+print(subdirectories)
+for dir in subdirectories:
+    dir_name = parent_directory + "\\" + dir
+    if dir_name not in dir_blacklist:
+        print("Removing " + dir_name)
+        os.rmdir(dir_name)
